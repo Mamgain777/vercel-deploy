8e72a31cfb63f422a52e195d695902511e81ad45
Himanshu Mamgain <76837197+Mamgain777@users.noreply.github.com>
2024-04-02 12:05:36 +0530

    listen the port

--- routes/app.js
+++ routes/app.js
@@ -7,4 +7,8 @@ app.get('/', (req,res)=>{
     res.json({"msg": "Hello World to vercel!!"});
 });
 
+app.listen(port,()=>{
+console.log("server is running on port "+port)
+})
+
 module.exports = app;
